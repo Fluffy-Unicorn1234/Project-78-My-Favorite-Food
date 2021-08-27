9cda824c859c3bcc9f130541ef0cf2d9a57ebf8b
Fluffy-Unicorn1234 <73382065+Fluffy-Unicorn1234@users.noreply.github.com>
2021-08-27 16:11:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+update() {
+    var food = [
+        "https://iambaker.net/wp-content/uploads/2019/07/GrilledPotWedges1-blog.jpg",
+        "https://www.tasteofhome.com/wp-content/uploads/2019/11/Air-Fryer-Potato-Chips_EXPS_EDAF20_227041_C03_18_7b-6.jpg?fit=700,1024",
+        "https://media.timeout.com/images/105757031/image.jpg",
+        "https://static.fanpage.it/wp-content/uploads/sites/22/2020/09/iStock-618214356-1200x1200.jpg",
+        "https://lh3.googleusercontent.com/ksz1Txwisz2emMkhY0u-21mmhKZq6qxdWJYlDtuYa029M4JsoBpBGTwog1e8ZhyvYZKvdAY2G2SEYXyVQxxbmGurFg=w1000",
+        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-202104-airfryerhotdogs-044-1619472270.jpg?crop=0.912xw:0.862xh;0.00340xw,0.0536xh&resize=640:*",
+        "https://www.qsrmagazine.com/sites/default/files/styles/story_page/public/phut_0.jpg?itok=h30EAnkk",
+        "https://www.mcdonalds.com/is/image/content/dam/usa/nfl/nutrition/items/iconic/desktop/t-mcdonalds-qpc-deluxe-burger.jpg"
+    ]; 
+}
